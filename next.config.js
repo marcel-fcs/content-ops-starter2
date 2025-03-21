429adfb44aacd0c602c9e0c2bcd0e15d5c4bbd5e
marcel-fcs <marcel.eisenmann@ovgu.de>
2025-03-21 10:16:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@
  */
 const nextConfig = {
      output: 'export',
-    env: {
-        stackbitPreview: process.env.STACKBIT_PREVIEW
-    },
-    trailingSlash: true,
-    reactStrictMode: true
 };
 
 module.exports = nextConfig;
