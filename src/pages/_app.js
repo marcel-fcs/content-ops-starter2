84905b29d5633938b04e8cca917c60cbe162ddab
marcel-fcs <marcel.eisenmann@ovgu.de>
2025-03-26 15:24:17 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../css/main.css';
-import CookieConsentBanner from '../components/blocks/CookieConsentBanner/CookieConsentBanner.js' // Cookie-Banner importieren
+import CookieConsentBanner from '../components/blocks/CookieConsentBanner/CookieConsentBanner' // Cookie-Banner importieren
 
 export default function MyApp({ Component, pageProps }) {
     return (
