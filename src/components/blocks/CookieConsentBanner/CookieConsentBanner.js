56f273517161a837344db6950177a7e2713966c6
marcel-fcs <marcel.eisenmann@ovgu.de>
2025-03-26 16:09:57 +0100

    Update CookieConsentBanner.js

--- src/components/blocks/CookieConsentBanner/CookieConsentBanner.js
+++ src/components/blocks/CookieConsentBanner/CookieConsentBanner.js
@@ -7,24 +7,23 @@ const CookieConsentBanner = () => {
     <CookieConsent
       location="bottom"
       buttonText="Akzeptieren"
-      declineButtonText="Ablehnen"
+      //declineButtonText="Ablehnen"
       enableDeclineButton
       cookieName="CookieConsent"
       style={{ background: "#2B373B", color: "#FFF" }}
       buttonStyle={{ backgroundColor: "#4CAF50", color: "#FFF", fontSize: "14px" }}
-      declineButtonStyle={{ backgroundColor: "#f44336", color: "#FFF", fontSize: "14px" }}
+     // declineButtonStyle={{ backgroundColor: "#f44336", color: "#FFF", fontSize: "14px" }}
       expires={365}  // Number of days before the cookie expires
       onAccept={() => {
         // Add functionality when user accepts cookies
         console.log("Cookies accepted");
       }}
-      onDecline={() => {
+    /*  onDecline={() => {
         // Add functionality when user declines cookies
         document.cookie = "user_cookie_consent=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=https://fliegerclub-sbk.netlify.app;"
         document.cookie = "other_cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=https://fliegerclub-sbk.netlify.app;"
         console.log("Cookies declined");
-      }}
-    >
+      }}*/    >
       Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies. 
       Bei Cookies handelt es sich um kleine Textdateien, die auf Ihrer Festplatte dem von Ihnen verwendeten Browser zugeordnet gespeichert werden und durch welche der Stelle,
       die den Cookie setzt (hier durch uns), bestimmte Informationen zufließen. 
