7489b83588856ff10be59ecde47445f092b0de75
marcel-fcs <marcel.eisenmann@ovgu.de>
2025-04-10 10:56:53 +0200

    Update index.tsx new tab

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -15,7 +15,7 @@ export default function Action(props) {
     return (
         <Link
             href={url}
-            {newtab ? target="_blank" : null}
+            {target= {newtab && "_blank"}
             aria-label={altText}
             id={elementId}
             className={classNames(
